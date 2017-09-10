d45b63ba5ff2619f39385ea81f7edd143e1dfc55
Igor360 <igor.dawidencko@gmail.com>
2017-09-10 17:24:39 +0300

    Lab_1
    - add lab to repository

---
+++ LAB_1/src/main/java/helloWorld.java
@@ -0,0 +1,6 @@
+public class helloWorld { // simple class that show message hello world
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!!"); // show message
+    }
+}
